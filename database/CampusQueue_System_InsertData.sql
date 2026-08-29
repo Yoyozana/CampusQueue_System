c9dd0d0033fdae77ae90c120610d226d052cd4a8
Nokubongwa Nontando Ngobese <nokubongwangobese751@gmail.com>
2026-08-29 12:14:08 +0200

    Create CampusQueue_System_InsertData.sql

---
+++ database/CampusQueue_System_InsertData.sql
@@ -0,0 +1,9 @@
+INSERT INTO services (service_name, description, location)
+VALUES
+('Registration', 'Student registration services', 'Main Administration Building'),
+
+('Financial Aid', 'NSFAS and financial assistance', 'Student Centre'),
+
+('Academic Advising', 'Academic support and advising', 'Faculty Building'),
+
+('Student Accommodation', 'Accommodation assistance', 'Residence Office');
