7bfbe899299f69cb7eb9b09d9fd3a160d7e69065
esethustuart-creator <esethustuart@gmail.com>
2026-08-30 02:00:50 +0200

    Update CampusQueue_System_InsertData.sql

--- database/CampusQueue_System_InsertData.sql
+++ database/CampusQueue_System_InsertData.sql
@@ -28,28 +28,6 @@ VALUES
 (3, 'Academic Advising Queue', 'open'),
 (4, 'Accommodation Queue', 'open');
 
-
-INSERT INTO services (service_name, description, location)
-VALUES
-('Registration', 'Student registration services', 'Main Administration Building'),
-
-('Financial Aid', 'NSFAS and financial assistance', 'Student Centre'),
-
-('Academic Advising', 'Academic support and advising', 'Faculty Building'),
-
-('Student Accommodation', 'Accommodation assistance', 'Residence Office');
-
-
-INSERT INTO queues (service_id, queue_name, status)
-VALUES
-(1, 'Registration Queue', 'open'),
-
-(2, 'Financial Aid Queue', 'open'),
-
-(3, 'Academic Advising Queue', 'open'),
-
-(4, 'Accommodation Queue', 'open');
-
 -- Insert Queue Tickets
 INSERT INTO queue_tickets
 (user_id, queue_id, ticket_number, position, status)
